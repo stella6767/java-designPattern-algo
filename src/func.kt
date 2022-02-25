3f28bef47670c42a63fad8e505d07c42afe00aba
kang <alsrb9434@gmail.com>
2022-02-25 17:12:37 +0900

    재귀함수 이상?

--- src/func.kt
+++ src/func.kt
@@ -2,6 +2,7 @@
  * 함수 문법
  *
  * 반환타입이 없으면 Unit, 반환타입이 있으면, Int, String 같은 타입들을 적어주고 return
+ * Unit은 따로 명시해주지 않아도 되나보다.
  */
 
 fun play(num:Int): Unit {
@@ -48,12 +49,58 @@ fun add3(n1:Int, n2:Int):Int{
 }
 
 
-/**
- * 명명된 인수 사용
- */
+
 fun printN1(n1: Int = 10) : Unit{
+    /**
+     * 명명된 인수 사용
+     */
+
     println(n1)
 }
+
+
+
+
+fun <T> test(num:Int, vararg  items: T) {
+
+    /**
+     * 함수 오버로딩의 한계를 극복하기 위한 가변인수 사용법,  vararg (자바에도 가변인수 사용가능하지만..)
+     * , T = 제네릭, 요건 똑같고
+     */
+
+    for(item in items){
+        print("${item} ")
+    }
+}
+
+
+
+tailrec fun printData(endNum:Int, initNum:Int):Unit {
+
+    /**
+     * 재귀 함수
+     *
+     * 왠만하면 앞에 talirec 붙이는 게 좋다.
+     * 왜냐면 자바코드로 변경될 때, 재귀함수로 변경되지 않고, 일반적인 while문으로 변경되기 때문
+     *
+     * tailrec을 사용할 때 주의점은 재귀함수가 함수의 가장 마지막에서 호출되어야 한다
+     */
+    //println(initNum) 뭐지?
+
+
+    if(endNum == initNum){
+        return
+    }else{
+        println("Hello World")
+    }
+
+
+    printData(endNum, initNum+1)
+}
+
+
+
+
 
 
 fun main() {
@@ -64,8 +111,15 @@ fun main() {
     printN1(30) //오버로딩
     printN1(n1 = 20) //명명된 인수
 
+    test(19, "이봐 ")
+    test(19, "hi","im","a kang" , 66)
+
+    printData(3, 1)
+
 
 }
+
+
 
 
 
