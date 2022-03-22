52b61dbfa4d0482cbfef8abbe101a8c13a4f1d2e
kang <alsrb9434@gmail.com>
2022-03-22 20:10:29 +0900

    flyweight pattern

---
+++ src/design/flyweight/Character.java
@@ -0,0 +1,15 @@
+package design.flyweight;
+
+public class Character {
+
+    char value;
+
+    String color;
+    Font font;
+
+    public Character(char value, String color, Font font) {
+        this.value = value;
+        this.color = color;
+        this.font = font;
+    }
+}
